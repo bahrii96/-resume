cf4b7f6b67fb8f0d5a4bda2427c2f0d30e294f76
bahrii96 <81860681+bahrii96@users.noreply.github.com>
2021-12-08 00:28:52 +0300

    добавив настройку беграонду

--- js/script.js
+++ js/script.js
@@ -13,3 +13,77 @@ $(document).ready(function () {
         centerMode:true
     });
 })
+const settingsBox=document.querySelector(".settings")
+const settings = document.querySelector(".btn-settings");
+settings.addEventListener("click", settingsActive)
+function settingsActive() {
+    if (
+      settingsBox.classList.contains("settings-active")  
+    ) {
+         settingsBox.classList.remove("settings-active");
+    }
+   else
+    settingsBox.classList.add("settings-active");
+}
+ 
+// зміна беграонду!!!!!!!!!
+// const color = {
+//   blue:  document.querySelector(".blue"),
+//    maroon: document.querySelector(".maroon"),
+//    saddleBrown: document.querySelector(".saddleBrown"),
+//    indigo: document.querySelector(".indigo"),
+//    purple: document.querySelector(".purple"),
+//    DarkSlateGrey: document.querySelector(".DarkSlateGrey")
+// }
+const color = document.querySelector(".settings__list")
+console.log(color);
+const blockColor = {
+hero:document.querySelector(".hero__box"),
+skills:document.querySelector(".skills"), 
+    form:document.querySelector(".form")
+  
+//    DarkSlateGrey: document.querySelector(".DarkSlateGrey")   
+}
+// console.log(blockColor.hero.classList);
+
+let colorAddReove=""
+color.addEventListener('click', catchElement)
+function catchElement(event) {
+   
+    if (event.target.nodeName !== 'BUTTON') {
+        return
+    }
+    const classEl = event.target.classList[1];
+    // console.log(classEl);
+    
+    addClassOnElement(classEl)
+}
+
+function addClassOnElement(classEl) {
+     
+    const values = Object.values(blockColor);
+    if (colorAddReove!=="") {
+     values.map(el =>
+        el.classList.remove(`${colorAddReove}`));   
+    }
+    // values.map(el =>
+    //     el.classList.remove(`${colorAddReove}`));
+console.log(colorAddReove);
+    values.map(el => el.classList.add(`${classEl}`))
+    colorAddReove=classEl
+    console.log(values);
+    
+     
+
+  
+    
+    
+
+    // console.log(values);
+
+//     blockColor.hero.classList.add("blue");
+//     blockColor.education.forEach.call(blockColor.education, function (el) {
+//         el.classList.add("blue");
+//     });
+// blockColor.form__button.classList.add("blue");
+}
